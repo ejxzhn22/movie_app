616de10c00c2950a4f1531406d56f8b74a7dbfb7
ejxzhn22 <ejxzhn22@naver.com>
2020-08-30 17:26:22 +0900

    Updates

---
+++ precache-manifest.a0ce1c08d2f848b7253555d0d35b0773.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c87f2dcbf59cde332462e32572b237",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "50ab648f7613378dd256",
+    "url": "/movie_app/static/css/main.7841db40.chunk.css"
+  },
+  {
+    "revision": "964546851f2431c5d268",
+    "url": "/movie_app/static/js/2.2d3cfcc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.2d3cfcc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50ab648f7613378dd256",
+    "url": "/movie_app/static/js/main.85c64519.chunk.js"
+  },
+  {
+    "revision": "ffbd4de4896f1a599175",
+    "url": "/movie_app/static/js/runtime-main.0186e20d.js"
+  }
+]);
